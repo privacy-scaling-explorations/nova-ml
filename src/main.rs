ec2508054ab3b2da2a7071017c3936a672467a9c
drCathieSo.eth <socathie@users.noreply.github.com>
2023-06-21 00:40:45 +0800

    feat: add cli argument to `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
+use std::env;
+
+// mod merkle;
 mod recursive;
-// mod conv2d;
-// mod conv2d_nova;
+mod conv2d;
+mod conv2d_nova;
 
 fn main() {
-    recursive::main();
-    // conv2d::main();
-    // conv2d_nova::main();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Please provide an example name");
+        return;
+    }
+
+    let example = &args[1];
+
+    if example == "recursive" {
+        recursive::main();
+    } else if example == "merkle" {
+        // merkle::main();
+    } else if example == "conv2d" {
+        conv2d::main();
+    } else if example == "conv2d_nova" {
+        conv2d_nova::main();
+    } else {
+        println!("Please provide a valid example name");
+    }
 }
